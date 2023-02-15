e3f916425c7ab240f3bd638cb45643836d92b0aa
Cactus <ysjungman@gmail.com>
2023-02-16 03:13:45 +0900

    리액트 event

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Profile from './Profile';
 import LightSwich from './LightSwich';
+import SampleButton from './SampleButton';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <LightSwich></LightSwich>
-        <Profile></Profile>
+        <SampleButton></SampleButton>
       </div>
     );
   }
