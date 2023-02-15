4197d183c80dd2e3c10ec2a9e5e10fc2eff73f7e
Cactus <ysjungman@gmail.com>
2023-02-16 03:47:24 +0900

    리액트 state evnet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import Profile from './Profile';
 import LightSwich from './LightSwich';
-import SampleButton from './SampleButton';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SampleButton></SampleButton>
+        <LightSwich></LightSwich>
       </div>
     );
   }
--- src/LightSwich.js
+++ src/LightSwich.js
@@ -3,15 +3,23 @@ import React, { Component } from 'react';
 class LightSwich extends Component {
   constructor() {
     super()
+    this.handelClick = this.handelClick.bind(this)
     this.state = {
       power: false
     }
   }
 
+  handelClick() {
+    this.setState({
+      power: !this.state.power
+    })
+  }
+
   render() {
     return (
       <div>
-        {this.state.power ? 'ON' : 'OFF'}
+        <p>{this.state.power ? 'ON' : 'OFF'}</p>
+        <button onClick={this.handelClick}>Swich</button>
       </div>
     )
   }
